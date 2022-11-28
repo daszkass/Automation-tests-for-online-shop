f734239480868d3a2fabd5c39dacba81ab416b7b
Daria <daszka_s@interia.pl>
2022-11-28 23:14:50 +0100

    Add features, step definitions, readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# TA-Homework-e2e
+## How to test
+- Install node.js
+- Clone repository
+- Run: `yarn install` command to install all dependencies
+- Run: `yarn wdio` command to run tests
---
+++ features/step-definitions/bootcamp.js
@@ -0,0 +1,47 @@
+const { Given, When, Then } = require('@wdio/cucumber-framework');
+
+Given("I am on the home page", async () => {
+    await browser.url('/');
+    await browser.pause(3000);
+});
+
+When("I enter {string} in the search bar", async (searchWord) => {
+    await $('.header2021-search-inner input[type="search"]').setValue(searchWord);
+});
+
+When("I click the search button", async () => {
+    try {
+        await $('.header2021-search-button button').click();
+    } catch (error) {
+        await $('#modal-Website .close').click();
+        await $('.header2021-search-button button').click();
+    }
+});
+
+Then("At least {int} item should appear", async (amount) => {
+    const items = await $$('.item-cell');
+    await expect(items).toBeElementsArrayOfSize({ gte:amount });
+});
+
+Then("Delete browser cookies", async () => {
+    browser.deleteCookies();
+});
+
+When("I open {string} tab", async (tabName) => {
+    const tab = await $('.header2021').$(`.font-s=${tabName}`);
+    try {
+        await tab.click();
+    } catch (error) {
+        await $('#modal-Website .close').click();
+        await tab.click();
+    }
+});
+
+When("I click on the shop logo", async () => {
+    const logo = await $('.header2021-logo-img');
+    await logo.click();
+});
+
+Then("Main page should open", async () => {
+    await expect(browser).toHaveUrl('https://www.newegg.com/');
+});
